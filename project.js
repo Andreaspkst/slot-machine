618357ea8637bd25adde308b681c8ca3ec66c132
Andreas Papakostas <andreaspkst@gmail.com>
2024-04-08 18:23:07 +0300

    Completed Deposit Function

--- project.js
+++ project.js
@@ -6,3 +6,20 @@
 // 6. Give the user their winnings
 // 7. Play again
 
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while(true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
